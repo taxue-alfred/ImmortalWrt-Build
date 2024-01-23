3b0052bde5a486d3f06883fc3d72d04c03a6a41b
踏雪Vernon <35928119+taxue-alfred@users.noreply.github.com>
2024-01-23 15:49:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,11 +11,10 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Enable r8125 ASPM
-cp -f $GITHUB_WORKSPACE/010-config.patch package/kernel/r8125/patches/010-config.patch
+# cp -f $GITHUB_WORKSPACE/010-config.patch package/kernel/r8125/patches/010-config.patch
 
 #Apply the patches
 git apply $GITHUB_WORKSPACE/patches/*.patch
