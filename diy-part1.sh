452ba0b3144b7ecd70275614d41037cede75ee66
踏雪Vernon <35928119+taxue-alfred@users.noreply.github.com>
2024-01-27 16:45:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,14 +16,14 @@
 # Add a feed source
 # sed -i '$a src-git mosdns https://github.com/Gzxhwq/openwrt-mos;dev' feeds.conf.default
 # sed -i '$a src-git my_packages https://github.com/Gzxhwq/openwrt-packages' feeds.conf.default
-sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+# sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
 
 #Add external packages
 # git submodule add https://github.com/Gzxhwq/openwrt-packages.git package/others/my_packages
 # sed -i '/iptables-mod-socket/d' ./package/others/my_packages/sing-box/Makefile
 
-git submodule add https://github.com/jerrykuku/luci-app-argon-config.git package/others/luci-app-argon-config
-git submodule add https://github.com/jerrykuku/luci-theme-argon.git package/others/luci-theme-argon
+# git submodule add https://github.com/jerrykuku/luci-app-argon-config.git package/others/luci-app-argon-config
+# git submodule add https://github.com/jerrykuku/luci-theme-argon.git package/others/luci-theme-argon
 # git submodule add https://github.com/ophub/luci-app-amlogic.git package/others/luci-app-amlogic
-git submodule add https://github.com/xiaorouji/openwrt-passwall.git package/others/luci-app-passwall
+# git submodule add https://github.com/xiaorouji/openwrt-passwall.git package/others/luci-app-passwall
 # git submodule add https://github.com/xiaorouji/openwrt-passwall2.git package/others/luci-app-passwall2
